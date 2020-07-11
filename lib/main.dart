7916c1a13455398f250e4e40b024aef3b44e3dc1
eminakcil <eminakcil@outlook.com>
2020-07-11 23:53:28 +0300

    disable screen rotation

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
         debugShowCheckedModeBanner: false, theme: ThemeData(primarySwatch: Colors.blueGrey), home: TempPage());
   }

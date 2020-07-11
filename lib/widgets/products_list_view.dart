98b1805fa18629394a1e645a03342991a9ed585c
eminakcil <eminakcil@outlook.com>
2020-07-11 21:30:13 +0300

    product list on pressed route

--- lib/widgets/products_list_view.dart
+++ lib/widgets/products_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:opazar/models/DaP.dart';
+import 'package:opazar/screens/product_page.dart';
 
 class ProductsListView extends StatelessWidget {
   final List<DaP> dapList;
@@ -32,53 +33,60 @@ class ListViewItem extends StatelessWidget {
       ),
       height: 130,
       margin: EdgeInsets.only(bottom: 8),
-      padding: EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            width: 190,
-            child: CachedNetworkImage(
-              imageUrl: dap.product.imageUrl,
-              imageBuilder: (context, imageProvider) => Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: imageProvider,
-                    fit: BoxFit.cover,
+      child: RawMaterialButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => ProductPage(dap: dap)));
+        },
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 190,
+                child: CachedNetworkImage(
+                  imageUrl: dap.product.imageUrl,
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      ),
+                    ),
                   ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
+                  placeholder: (context, url) => Center(child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 130,
+                  margin: EdgeInsets.only(left: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(dap.product.name, style: TextStyle(fontSize: 22.0)),
+                      Text(dap.category.name, style: TextStyle(fontSize: 16.0)),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(dap.product.unit, style: TextStyle(fontSize: 16.0)),
+                          Text(dap.product.priceText, style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w700)),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
-              placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            ),
+            ],
           ),
-          Expanded(
-            child: Container(
-              height: 130,
-              margin: EdgeInsets.only(left: 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(dap.product.name, style: TextStyle(fontSize: 22.0)),
-                  Text(dap.category.name, style: TextStyle(fontSize: 16.0)),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(dap.product.unit, style: TextStyle(fontSize: 16.0)),
-                      Text(dap.product.priceText, style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w700)),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
